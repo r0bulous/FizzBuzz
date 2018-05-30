e1cff730c9f4619a09dd05d8ae25caea35e55add
r0bulous <rrwinn@gmail.com>
2018-05-30 15:31:27 -0400

    initial commit

---
+++ FizzBuzz.Tests/FizzBuzzerTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Tests
+{
+    [TestFixture]
+    class FizzBuzzerTest
+    {
+        [Test]
+	public void Get_Returns_1_for_1()
+	{
+		var fizzBuzzer = new FizzBuzzer();
+
+		var result = fizzBuzzer.Get(1);
+
+		Assert.That(result, Is.EqualTo("1"));
+	}	
+}
+    }
+
---
+++ FizzBuzz/FizzBuzzer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzer
+    {
+        public string Get(int num)
+        {
+            return "1";
+        }
+    }
+}
